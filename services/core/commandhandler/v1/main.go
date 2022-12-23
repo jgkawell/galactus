4fea993f4d5cf26670cce38f43db3c3cb191e46e
Jack Kawell <jgkawell@outlook.com>
2022-12-23 09:55:20 -0600

    Updated commandhandler to use cf.Create()

--- services/core/commandhandler/v1/main.go
+++ services/core/commandhandler/v1/main.go
@@ -1,22 +1,16 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
 	h "commandhandler/handler"
 	s "commandhandler/service"
 
-	agpb "github.com/jgkawell/galactus/api/gen/go/core/aggregates/v1"
 	pb "github.com/jgkawell/galactus/api/gen/go/core/commandhandler/v1"
 	espb "github.com/jgkawell/galactus/api/gen/go/core/eventstore/v1"
 	rgpb "github.com/jgkawell/galactus/api/gen/go/core/registry/v1"
 
 	"github.com/jgkawell/galactus/pkg/chassis"
 	cf "github.com/jgkawell/galactus/pkg/chassis/clientfactory"
-	ct "github.com/jgkawell/galactus/pkg/chassis/context"
 
-	"github.com/google/uuid"
 	"google.golang.org/grpc"
 )
 
@@ -38,28 +32,11 @@ func main() {
 			CreateInternalClients: func(b chassis.MainBuilder, clientFactory cf.ClientFactory) []*grpc.ClientConn {
 				var (
 					conn        *grpc.ClientConn
-					err         error
 					connections []*grpc.ClientConn
 				)
-
-				ctx, _ := ct.NewExecutionContext(context.Background(), b.GetLogger(), uuid.NewString())
-				connectionResponse, err := b.GetRegistryClient().Connection(ctx.GetContext(), &rgpb.ConnectionRequest{
-					Name:    "eventstore",
-					Version: "0.0.0",
-					Type:    agpb.ProtocolKind_PROTOCOL_KIND_GRPC,
-				})
-
-				if err != nil {
-					b.GetLogger().WithError(err).Fatal("failed to get eventstore connection")
-				}
-
-				fullAddress := fmt.Sprintf("%s:%d", connectionResponse.GetAddress(), connectionResponse.GetPort())
-				esc, conn, err = clientFactory.CreateEventStoreClient(b.GetLogger(), fullAddress)
-				if err != nil {
-					b.GetLogger().WithError(err).Panic("failed to connect to eventstore service")
-				}
+				conn = clientFactory.Create(b.GetLogger(), rgpb.Registry_ServiceDesc)
+				esc = espb.NewEventStoreClient(conn)
 				connections = append(connections, conn)
-
 				return connections
 			},
 		},
