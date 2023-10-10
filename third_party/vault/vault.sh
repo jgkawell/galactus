e910011d303714408aabcd7c31f3dd186b3871e2
Jack Kawell <jgkawell@outlook.com>
2023-10-09 20:50:57 -0500

    added vault example

---
+++ third_party/vault/vault.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export VAULT_TOKEN=myroot
+export VAULT_ADDR=http://0.0.0.0:8200
+
+vault kv put -mount=secret core/database/postgres/registry url=postgres://admin:admin@localhost:5434/dev?sslmode=disable
+vault kv put -mount=secret core/database/mongo/registry url=mongodb://admin:admin@localhost:27017
+vault kv put -mount=secret core/messaging/rabbitmq/registry url=amqp://guest:guest@localhost:5672
