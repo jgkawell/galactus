e2888f8bbf8e6cc56544da15128b72be6a5792ea
Jack Kawell <jgkawell@outlook.com>
2023-10-09 20:52:24 -0500

    update gctl to manage api

--- tools/gctl/cmd/api/clean.go
+++ tools/gctl/cmd/api/clean.go
@@ -19,27 +19,9 @@ func Clean(cmd *cobra.Command, args []string) (err error) {
 
 	// remove buf.lock
 	os.Remove(filepath.Join(apiPath, "buf.lock"))
-	// clean docs
-	output.Println("Cleaning docs...")
-	os.Remove(filepath.Join(apiPath, "gen", "docs", "docs.md"))
-	os.Remove(filepath.Join(apiPath, "gen", "docs", "index.html"))
 	// clean go
 	output.Println("Cleaning go...")
 	err = deleteAllSubDirectories(filepath.Join(apiPath, "gen", "go"))
-	if err != nil {
-		output.Error(err)
-		return err
-	}
-	// clean openapiv2
-	output.Println("Cleaning openapiv2...")
-	err = deleteAllSubDirectories(filepath.Join(apiPath, "gen", "openapiv2"))
-	if err != nil {
-		output.Error(err)
-		return err
-	}
-	// clean web
-	output.Println("Cleaning web...")
-	err = deleteAllSubDirectories(filepath.Join(apiPath, "gen", "web"))
 	if err != nil {
 		output.Error(err)
 		return err
